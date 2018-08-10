eb93698ec2dc842e54e340b3f00ebe1742fc2af9
fujisada <fujihashi@sencorp.co.jp>
2018-08-10 15:04:58 +0900

    add id to specify execute sequence

--- bin/array_execute.sh
+++ bin/array_execute.sh
@@ -5,6 +5,9 @@ item=$(cat << EOS
     "id" : {
         "S" : "$(date +%s)_$(echo $(( $RANDOM )))"
     },
+    "seq_id" : {
+        "S" : "$SEQ_ID"
+    },
     "job_array_index" : {
         "N" : "$AWS_BATCH_JOB_ARRAY_INDEX"
     },
