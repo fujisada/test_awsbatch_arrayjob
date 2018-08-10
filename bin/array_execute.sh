9d5b33e8a8e7531e58bb69f5d04148f1f6127905
fujisada <fujihashi@sencorp.co.jp>
2018-08-10 14:57:01 +0900

    store record to dynamodb

--- bin/array_execute.sh
+++ bin/array_execute.sh
@@ -1,3 +1,22 @@
 #!/bin/bash
 
-echo "aaa";
+tablename=$DYNAMO_TABLE
+item=$(cat << EOS
+{
+    "id" : {
+        "S" : "$(date +%s)_$(echo $(( $RANDOM )))"
+    },
+    "job_array_index" : {
+        "N" : "$AWS_BATCH_JOB_ARRAY_INDEX"
+    },
+    "unixtime" : {
+        "N" : "$(date +%s)"
+    }
+}
+EOS
+)
+
+echo "put item: $item"
+aws dynamodb put-item \
+    --table-name test_batcharray \
+    --item "$item"
